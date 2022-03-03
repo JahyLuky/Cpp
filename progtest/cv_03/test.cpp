6909f8a743d45ca7839006bbfe9a7c2c739bf705
Lukáš Jahelka <jahelluk@fit.cvut.cz>
2022-03-03 21:42:15 +0100

    halfway done

--- progtest/cv_03/test.cpp
+++ progtest/cv_03/test.cpp
@@ -12,16 +12,110 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
+
+bool findThem ( const string & query, const string & data, ostream & out )
+{
+
+
+  return false;
+}
+
+bool isPhoneNum ( const string & words )
+{
+  int len = words.size();
+  if ( len != 9 )
+  {
+    return false;
+  }
+  for ( int i = 0; i < len; i++ )
+  {
+    if ( words[i] >= '0' && words[i] <= '9' )
+    {
+      continue;
+    }
+    else if ( words[0] == '0' )
+    {
+      return false;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool checkData ( const string & line, string & data )
+{
+  stringstream toWord ( line );
+  string words;
+  int len, cnt = 0;
+
+  while( toWord >> words )
+  {
+    len = words.size();
+    if ( cnt == 2 )
+    {
+      if ( isPhoneNum( words ) == 0 )
+      {
+        return false;
+      }
+      words.insert( len, "\n" );
+      data = words;
+      cnt = 0;
+    }
+    else
+    {
+      words.insert( len, " " );
+      data = words;
+      cnt++;
+    }
+    cout << data;
+  }
+  
+  return true;
+}
+
 bool report ( const string & fileName, ostream & out )
 {
-  // TODO
-  return false;
+  ifstream ifs;
+  ifs.open( fileName );
+
+  string buffer, data;
+  int flag = 0;
+  while ( getline( ifs, buffer ) )
+  {
+    if ( buffer[0] == '\0' )
+    {
+      flag = 1;
+    }
+    if ( flag == 0 )
+    {
+      if ( checkData( buffer, data ) == false )
+      {
+        return false;
+      }
+    }
+    else
+    {
+      if ( findThem( buffer, data, out ) == false )
+      {
+        return false;
+      }
+    }
+  }
+
+  ifs.close();
+  return true;
 }
 
 #ifndef __PROGTEST__
 int main ()
 {
   ostringstream oss;
+  report ( "tests/test0_in.txt", oss );
+  //report ( "tests/test1_in.txt", oss );
+  /*
   oss . str ( "" );
   assert ( report( "tests/test0_in.txt", oss ) == true );
   assert ( oss . str () ==
@@ -36,6 +130,7 @@ int main ()
     "-> 1\n" );
   oss . str ( "" );
   assert ( report( "tests/test1_in.txt", oss ) == false );
+  */
   return 0;
 }
 #endif /* __PROGTEST__ */
