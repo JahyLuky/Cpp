7780cea54f1132d5edf65b7fe6e99789b61cc0d7
Lukáš Jahelka <jahelluk@fit.cvut.cz>
2022-03-13 11:32:28 +0100

    cv_04

--- progtest/cv/cv_04/test.cpp
+++ progtest/cv/cv_04/test.cpp
@@ -15,60 +15,69 @@ using namespace std;
 class CTimeStamp
 {
   public:
-    CTimeStamp ( int year, int month, int day, 
-                 int hour, int min, int sec )
-    : m_year ( year ),
-      m_month ( month ),
-      m_day ( day ),
-      m_hour ( hour ),
-      m_min ( min ),
-      m_sec ( sec )
-    {}
+    CTimeStamp();
+    CTimeStamp( int year, int month, int day,
+                int hour, int min, int sec )
+    {
+      m_data.push_back(year);
+      m_data.push_back(month);
+      m_data.push_back(day);
+      m_data.push_back(hour);
+      m_data.push_back(min);
+      m_data.push_back(sec);
+      //m_data[0] = year;
+      //m_data[1] = month;
+      //m_data[2] = day;
+      //m_data[3] = hour;
+      //m_data[4] = min;
+      //m_data[5] = sec;
+    }
+    const CTimeStamp & getDate ( void )
+    {
+      return *this;
+    }
   private:
-    int m_year;
-    int m_month;
-    int m_day;
-    int m_hour;
-    int m_min;
-    int m_sec;
-}
+    vector <int> m_data;
+};
 
 class CContact
 {
   public:
-    CContact ( CTimeStamp getTimeStamp, int a_phone, int b_phone )
-    : m_TimeStamp ( getTimeStamp ), m_a ( a_phone ), m_b ( b_phone )
+    CContact();
+    CContact( const CTimeStamp getTime, const int a_phone,
+     const int b_phone )
+    : m_date( getTime ), m_a( a_phone ), m_b( b_phone )
     {}
   private:
-    CTimeStamp & m_TimeStamp;
+    CTimeStamp m_date;
     int m_a;
     int m_b;
-}
+};
 
 class CEFaceMask
 {
   public:
-    CEFaceMask & addContact ( CTimeStamp contact, CContact a, CContact b )
+    CEFaceMask();
+    CEFaceMask & addContact( const CContact a )
     {
-      m_list[a] = b;
-      return * this;
+      m_data.push_back(a);
+      return *this;
     }
-    const CContact listContacts ( CContact a, CTimeStamp b , CTimeStamp c ) const
+    vector <int> & listContacts( int a_phone, CTimeStamp & start,
+    CTimeStamp & end )
     {
 
     }
   private:
-    CContact & m_a;
-    CContact & m_b;
-    CTimeStamp & m_contact;
-    vector <int> m_list;
-}
+    vector <CContact> m_data;
+};
+
 
 #ifndef __PROGTEST__
 int main ()
 {
   CEFaceMask test;
-
+  CContact(CTimeStamp(1,2,3,4,5,6),123,321);
   test . addContact ( CContact ( CTimeStamp ( 2021, 1, 10, 12, 40, 10 ), 123456789, 999888777 ) );
   test . addContact ( CContact ( CTimeStamp ( 2021, 1, 12, 12, 40, 10 ), 123456789, 111222333 ) )
        . addContact ( CContact ( CTimeStamp ( 2021, 2, 5, 15, 30, 28 ), 999888777, 555000222 ) );
@@ -81,6 +90,9 @@ int main ()
   assert ( test . listContacts ( 123456789, CTimeStamp ( 2021, 1, 5, 18, 0, 0 ), CTimeStamp ( 2021, 2, 21, 18, 0, 0 ) ) == (vector<int> {999888777, 111222333, 456456456}) );
   assert ( test . listContacts ( 123456789, CTimeStamp ( 2021, 1, 5, 18, 0, 1 ), CTimeStamp ( 2021, 2, 21, 17, 59, 59 ) ) == (vector<int> {999888777, 111222333}) );
   assert ( test . listContacts ( 123456789, CTimeStamp ( 2021, 1, 10, 12, 41, 9 ), CTimeStamp ( 2021, 2, 21, 17, 59, 59 ) ) == (vector<int> {111222333}) );
+  
+  
+
   return 0;
 }
 #endif /* __PROGTEST__ */
